5cbe185c00d152b68e8d29d8e232ea3e097b960c
rivailJunior <rivail.rj@gmail.com>
2025-08-04 00:29:29 -0400

    feat: adding params to auth config

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -37,27 +37,20 @@ export const authConfig = {
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
   session: {
     strategy: 'jwt',
   },
-  // adapter: PrismaAdapter(db),
+  trustHost: true, // ✅ Add this line
+  // OR if you want to be more explicit:
+  // trustedHosts: ['mydailycoffee.rivailjunior.com'],
   callbacks: {
     session: ({ session, token }) => {
       return {
         ...session,
         user: {
           ...session.user,
-          id: token.sub, // Use token.sub as the user ID when using JWT strategy
+          id: token.sub,
         },
       };
     },
